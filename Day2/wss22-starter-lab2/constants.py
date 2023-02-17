ded452c4c0780e31c183fbe75eb6cd5bc8f51ebe
G. Aswin <aswin.g.rns@gmail.com>
2023-02-17 09:20:47 +0530

    removing duplicate files

--- Day2/wss22-starter-lab2/constants.py
+++
@@ -1,8 +0,0 @@
-from typing import Final
-
-LOGFILE: Final[str] = "/tmp/wc.log"
-N_WORKERS: Final[int] = 20
-GLOB: Final[str] = "data/*.txt"
-IN: Final[bytes] = b"files"
-FNAME: Final[bytes] = b"fname"
-COUNT: Final[bytes] = b"count"
